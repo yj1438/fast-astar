20b15de54572d2a0aa901385b52d96737b9bd045
沐瑜 <yj138290@antfin.com>
2024-02-06 16:53:00 +0800

    readme

--- tests/base.js
+++ tests/base.js
@@ -3,17 +3,14 @@ const obj = require('../dist/index'),
     Astar = obj.Astar;
 
 // Create a grid
-let grid = new Grid({
-        col:11,                  // col
-        row:7,                   // row
-        render:function(){       // Optional, this method is triggered when the grid point changes
-            // console.log(this);
-        }
-    });
+const grid = new Grid({
+    col: 11,                  // col
+    row: 7,                   // row
+});
 
 // Add obstacles to the grid
-[[5,2],[5,3],[5,4]].forEach(item => {
-    grid.set(item,'type',1);    // Values greater than 0 are obstacles
+[[5, 2], [5, 3], [5, 4]].forEach(item => {
+    grid.set(item, 'type', 1);    // Values greater than 0 are obstacles
 });
 
 const { col, row, data } = grid;
@@ -21,23 +18,43 @@ const { col, row, data } = grid;
 // Pass the grid as a parameter to the Astar object
 const astar = new Astar(grid);
 const path = astar.search(
-    [0,3],                   // start
-    [10,4],                   // end
-    {                        // option
-        rightAngle:false,    // default:false,Allow diagonal
-        optimalResult:true   // default:true,In a few cases, the speed is slightly slower
+    [0, 3],                   // start
+    [10, 4],                  // end
+    {
+        rightAngle: false,    // default:false,Allow diagonal
+        optimalResult: true   // default:true,In a few cases, the speed is slightly slower
     }
 );
 
+console.log('Result', path);
+
+/*
+[
+  [ 0, 3 ],  [ 1, 3 ],
+  [ 2, 3 ],  [ 3, 4 ],
+  [ 4, 5 ],  [ 5, 5 ],
+  [ 6, 5 ],  [ 7, 5 ],
+  [ 8, 4 ],  [ 9, 4 ],
+  [ 10, 4 ]
+]
+*/
+
+/*
+0 0 0 0 0 0 0 0 0 0 0 
+0 0 0 0 0 0 0 0 0 0 0 
+0 0 0 0 0 1 0 0 0 0 0 
+2 2 2 0 0 1 0 0 0 0 0 
+0 0 0 2 0 1 0 0 2 2 2 
+0 0 0 0 2 2 2 2 0 0 0 
+0 0 0 0 0 0 0 0 0 0 0
+*/
 path.forEach(item => {
-    grid.set(item,'type',2);
+    grid.set(item, 'type', 2);
 });
 
-console.log('Result',path);      // [[2,3],[3,2],[4,1],[5,1],[6,1],[7,2],[8,3]]
-
-for(let i=0;i<row;i++){
+for (let i = 0; i < row; i++) {
     let str = '';
-    for(let j=0;j<col;j++){
+    for (let j = 0; j < col; j++) {
         str += data[i][j].type + ' ';
     }
     console.log(str);
